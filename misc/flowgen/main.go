76ce0f9fe08fa5d8b12e6fbef4592c076ba0283b
Hiroki Shirokura <hiroki.shirokura@linecorp.com>
2022-06-26 20:34:20 +0900

    2022.06.26-20:34

--- misc/flowgen/main.go
+++ misc/flowgen/main.go
@@ -13,9 +13,8 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var config struct {
-	arg1 string
-	arg2 string
+var cliOpt struct {
+	configfile string
 }
 
 type IPFixMessage struct {
@@ -78,13 +77,13 @@ func newCommand() *cobra.Command {
 		Use:  "cmd",
 		RunE: appMain,
 	}
-	cmd.Flags().StringVar(&config.arg1, "arg1", "def1", "this is arg1")
-	cmd.Flags().StringVar(&config.arg2, "arg2", "def2", "this is arg2")
+	cmd.Flags().StringVarP(&cliOpt.configfile, "config", "c", "./config.yaml",
+		"Specifiy the configuration file")
 	return cmd
 }
 
 func appMain(cmd *cobra.Command, args []string) error {
-	fmt.Printf("arg1=%s, arg2=%s\n", config.arg1, config.arg2)
+	fmt.Printf("config=%s\n", cliOpt.configfile)
 
 	msg := IPFixMessage{
 		Header: IPFixHeader{
