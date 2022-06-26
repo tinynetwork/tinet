67a279b79b3663bd287e4dbedbf4eeee13ecd571
Hiroki Shirokura <hiroki.shirokura@linecorp.com>
2022-06-26 05:01:22 +0000

    2022.06.26-05:01

--- misc/ebpf_flow_stats/test.sh
+++ misc/ebpf_flow_stats/test.sh
@@ -3,3 +3,5 @@ set -xe
 ID=$(sudo docker run -td nicolaka/netshoot)
 sudo ./reload.py
 sudo docker exec -it $ID apk update
+sudo docker exec -it $ID curl ifconfig.io
+sudo docker exec -it $ID curl slank.dev
