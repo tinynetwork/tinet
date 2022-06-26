3b92489a54bb3ec9449422dca2b1d52c52f37349
Hiroki Shirokura <hiroki.shirokura@linecorp.com>
2022-06-26 08:48:35 +0000

    2022.06.26-08:48

--- misc/flowgen/main.go
+++ misc/flowgen/main.go
@@ -1,57 +1,78 @@
 package main
 
 import (
-    "fmt"
-    "math/rand"
-    "os"
-    "net"
-    "log"
-    "time"
+	"fmt"
+	"math/rand"
+	"net"
+	"os"
+	"time"
 
-    "github.com/k0kubun/pp"
-    "github.com/spf13/cobra"
+	"github.com/spf13/cobra"
 )
 
 var config struct {
-    arg1 string
-    arg2 string
+	arg1 string
+	arg2 string
 }
 
-func newCommand() *cobra.Command {
-    cmd := &cobra.Command{
-	Use:  "cmd",
-	RunE: appMain,
-    }
-    cmd.Flags().StringVar(&config.arg1, "arg1", "def1", "this is arg1")
-    cmd.Flags().StringVar(&config.arg2, "arg2", "def2", "this is arg2")
-    return cmd
+type IPFixMessage struct {
+	Version      uint16
+	Count        uint16
+	SysUptime    uint32
+	Timestamp    uint32
+	FlowSequence uint32
+	SourceID     uint32
+	FlowSets     []IPFixFlowSet
 }
 
-func appMain(cmd *cobra.Command, args []string) error {
-    fmt.Printf("arg1=%s, arg2=%s\n", config.arg1, config.arg2)
+type IPFixFlowSet struct {
+	ID       uint16
+	Length   uint16
+	Template IPFixFlowTemplate
+}
 
-    conn, err := net.Dial("udp", "127.0.0.1:8080")
-    if err != nil {
-        log.Fatalln(err)
-        os.Exit(1)
-    }
-    defer conn.Close()
+type IPFixFlowTemplate struct {
+	ID     uint16
+	Count  uint16
+	Fields []IPFixFlowTemplateField
+}
 
-    n, err := conn.Write([]byte("Ping"))
-    if err != nil {
-        log.Fatalln(err)
-        os.Exit(1)
-    }
-    pp.Println(n)
-
-    return nil
+type IPFixFlowTemplateField struct {
+	Type   uint16
+	Length uint16
 }
 
 func main() {
-    rand.Seed(time.Now().UnixNano())
-    command := newCommand()
-    err := command.Execute()
-    if err != nil {
-	os.Exit(1)
-    }
+	rand.Seed(time.Now().UnixNano())
+	command := newCommand()
+	err := command.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func newCommand() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:  "cmd",
+		RunE: appMain,
+	}
+	cmd.Flags().StringVar(&config.arg1, "arg1", "def1", "this is arg1")
+	cmd.Flags().StringVar(&config.arg2, "arg2", "def2", "this is arg2")
+	return cmd
+}
+
+func appMain(cmd *cobra.Command, args []string) error {
+	fmt.Printf("arg1=%s, arg2=%s\n", config.arg1, config.arg2)
+
+	conn, err := net.Dial("udp", "10.146.0.6:2100")
+	if err != nil {
+		return err
+	}
+	defer conn.Close()
+
+	if _, err = conn.Write([]byte("Ping")); err != nil {
+		return err
+	}
+
+	return nil
 }
